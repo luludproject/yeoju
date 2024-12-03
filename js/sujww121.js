99576fbf4d0da23873409eb3bbe99bd717c37edb
unknown <chlalstn9@gmail.com>
2024-12-03 16:40:21 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://goyang.jianhomecare.com",
-  logo: "https://goyang.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://siheung.jianhomecare.com",
+  logo: "https://siheung.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
