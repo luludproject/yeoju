0fc80cc68f5633b83347564d055ca250ae788844
luludproject <luludproject@gmail.com>
2024-12-07 15:17:09 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://ansan.jianhomecare.com",
-  logo: "https://ansan.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://hwaseong.jianhomecare.com",
+  logo: "https://hwaseong.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
