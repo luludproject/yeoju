8a393e3adf14d3bada290dc6e7ea11419956a2f8
luludproject <luludproject@gmail.com>
2024-12-09 13:39:47 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://pyungtaek.jianhomecare.com",
-  logo: "https://pyungtaek.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://uijeongbu.jianhomecare.com",
+  logo: "https://uijeongbu.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
